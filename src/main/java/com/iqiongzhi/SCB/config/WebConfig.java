eec607aa81e21fc21281db3799b3dcdb84ad4cc9
Hanserprpr <2041283903@qq.com>
2025-01-25 12:49:48 +0800

    UPDATE
    增加日志功能

--- src/main/java/com/iqiongzhi/SCB/config/WebConfig.java
+++ src/main/java/com/iqiongzhi/SCB/config/WebConfig.java
@@ -1,4 +1,21 @@
 package com.iqiongzhi.SCB.config;
 
-public class WebConfig {
+import com.iqiongzhi.SCB.config.LoggingInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 注册拦截器，并指定拦截路径
+        registry.addInterceptor(loggingInterceptor)
+                .addPathPatterns("/**"); // 拦截所有请求
+    }
 }
